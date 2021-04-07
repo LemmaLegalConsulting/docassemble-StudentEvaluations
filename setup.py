3206f059c46608af751ab305ee4d6e5a4364bed0
System Administrator <qsteenhuis@gmail.com>
2021-04-06 22:29:55 -0400

    Additional language fixes

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.StudentEvaluations',
-      version='0.0.2',
+      version='0.0.3',
       description=(''),
       long_description='# docassemble.StudentEvaluations\r\n\r\n\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@lemmalegal.com\r\n\r\n',
       long_description_content_type='text/markdown',
